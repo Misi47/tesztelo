ac14d6c3aa7e12efe09448afd0076325e6d73dc0
Misi47 <Csire07@gmail.com>
2024-12-04 11:53:16 +0100

    ok

---
+++ dogaa/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dogaa
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int mostaniev =2024;
+            Console.WriteLine("Add meg az évet amikor nyert");
+            string ev = Console.ReadLine();
+            int szam;
+            bool szame = int.TryParse(ev, out szam);
+
+            if (szame)
+            {
+                Console.WriteLine($"{ev} egy szám");
+            }
+            else
+            {
+                Console.WriteLine($"{ev} nem egy szám");
+            }
+
+
+
+            Console.WriteLine("Add meg  a nevét");
+            string nev = Convert.ToString(Console.ReadLine());
+   
+
+            Console.WriteLine("Add meg a születési és halálozási évet");
+            string szulhalev = Console.ReadLine();
+
+
+            if (szulhalev.Length == 9 && szulhalev[4] == '-')
+            {
+                Console.WriteLine("Jól írtad be");
+            }
+            else
+            {
+                Console.WriteLine("Rosszul írtad be");
+            }
+
+
+            int ev2 = Convert.ToInt32(ev);
+            int kul= mostaniev - ev2;
+            Console.WriteLine($"{kul} éve kapta meg a nobel díjat");
+
+            Console.WriteLine("Add meg milyen nemzetiségű");
+            string orszag = Console.ReadLine();
+
+            if(orszag=="")
+            {
+                Console.WriteLine("Nem adtál meg adatot");
+            }
+            else
+            {
+                Console.WriteLine("Adtál meg adatot");
+            }
+       
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ tajszam/Program.cs
@@ -0,0 +1,96 @@
+﻿using System;
+
+using System.Collections.Generic;
+
+using System.Linq;
+
+using System.Text;
+
+using System.Threading.Tasks;
+ 
+namespace teszteéscon
+
+{
+
+    class Program
+
+    {
+
+        static void Main(string[] args)
+
+        {
+
+            double szam;
+
+            string szöveg;
+
+            szöveg = Console.ReadLine();
+
+            bool siker = double.TryParse(szöveg, out szam);
+
+            Console.WriteLine(siker);
+
+            Console.WriteLine(szam);
+
+            int szam2 = 10;
+
+            string szoveg2 = Convert.ToString(szam2);
+
+            Console.WriteLine(szoveg2);
+
+            Console.ReadKey();
+
+            string taj;
+
+            int szum = 0;
+
+            int i, j;
+
+            taj = "467764647";
+
+            for (i = 0; i <= 7; i++)
+
+            {
+
+                if ((i + 1) % 2 == 1)
+
+                {
+
+                    j = int.Parse(taj[i].ToString());
+
+                    szum = szum + (j * 3);
+
+                }
+
+                else
+
+                {
+
+                    j = int.Parse(taj[i].ToString());
+
+                    szum = szum + (j * 7);
+
+                }
+
+            }
+
+            Console.WriteLine(szum);
+
+            int ellenorzes = szum % 10;
+
+            if (ellenorzes == int.Parse(taj[0].ToString())) ;
+
+            else
+
+            {
+
+                Console.WriteLine("Rossz taj");
+
+            }
+
+        }
+
+    }
+
+}
+
