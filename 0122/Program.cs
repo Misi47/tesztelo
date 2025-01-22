31ef91b977daaa00dbe75d3efa68ddab576a3e88
Misi47 <Csire07@gmail.com>
2025-01-22 11:05:23 +0100

    file

---
+++ 0122/Program.cs
@@ -0,0 +1,125 @@
+﻿using System;
+
+using System.Collections.Generic;
+
+using System.Linq;
+
+using System.Text;
+
+using System.Threading.Tasks;
+
+using System.IO;
+
+namespace ConsoleApp8
+
+{
+
+    class Program
+
+    {
+
+        struct Adat
+
+        {
+
+            public string nev;
+
+            public int oraber;
+
+            public string kolis;
+
+            public string lakhely;
+
+        }
+
+        static void Main(string[] args)
+
+        {
+
+            bool vanfile = File.Exists("nevek.txt");
+
+            if (vanfile)
+
+            {
+
+                string[] kategoriak = File.ReadAllLines("nevek.txt");
+
+
+                int i;
+
+                for (i = 0; i < kategoriak.Length; i++)
+
+                {
+
+                    Console.WriteLine(kategoriak[i]);
+
+                }
+
+                Adat[] adatok = new Adat[kategoriak.Length];
+
+                for (i = 1; i < kategoriak.Length; i++)
+
+                {
+
+                    string[] sorok = kategoriak[i].Split(' ');
+
+                    adatok[i].nev = sorok[0];
+
+                    try
+
+                    {
+
+                        adatok[i].oraber = int.Parse(sorok[1]);
+
+                    }
+
+                    catch (Exception e)
+                    {
+
+                        adatok[i].oraber = 0;
+
+                    }
+
+                    adatok[i].kolis = sorok[2];
+
+                    adatok[i].lakhely = sorok[3];
+
+                }
+
+                Console.WriteLine("Az adatok tömb elemei:");
+
+                for (i = 1; i < kategoriak.Length; i++)
+
+                {
+
+                    Console.WriteLine("{0}   {1}   {2}  {3}  ", adatok[i].nev, adatok[i].oraber, adatok[i].kolis, adatok[i].lakhely);
+
+                }
+
+                string[] vissza = new string[kategoriak.Length];
+                for (i = 0;i<kategoriak.Length;i++)
+                { 
+                    vissza[i] = adatok[i].nev + " "+ adatok[i].oraber+" " + adatok[i].kolis + " " + adatok[i].lakhely + " ";
+                }
+                Console.WriteLine("Az uj sting tömb");
+                for(i=0;i<kategoriak.Length;i++)
+                {
+                    Console.WriteLine(vissza[i]);
+                }
+                File.WriteAllLines("vissza.txt", vissza);
+
+            }
+
+            else
+
+                Console.WriteLine("nincs ilyen file");
+
+
+            Console.ReadKey();
+
+        }
+
+    }
+
+}
+
---
+++ doga/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace doga
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Adj meg két számot!");
+            Console.Write("Első szám: ");
+            int sz1 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Második szám: ");
+            int sz2 = Convert.ToInt32(Console.ReadLine());
+           
+
+            Console.WriteLine("Írd be a kívánt műveletet(+-*/)");
+            string muv = Console.ReadLine();
+            if (muv=="+"|| muv == "-" || muv == "*" || muv == "/") 
+            {
+                if (muv == "+")
+                {
+                    Console.WriteLine($"A két szám összege:" + (sz1 + sz2));
+                }
+                else if (muv == "-")
+                {
+                    Console.WriteLine($"A két szám különbsége:" + (sz1 - sz2));
+                }
+                else if (muv == "*")
+                {
+                    Console.WriteLine($"A két szám szorzata:" + (sz1 * sz2));
+                }
+                else if (muv == "/")
+                {
+                    if (sz2 == 0)
+                    {
+                        Console.WriteLine("Nullával nem lehet osztani");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"A két szám hányadosa:" + (sz1 / sz2));
+                    }
+
+                }
+            }
+            else 
+            {
+                Console.WriteLine("Nem érvényes műveletet adtál meg");
+            }
+            
+            Console.ReadKey();
+        }
+    }
+}
